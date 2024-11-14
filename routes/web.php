2fb45d78480523ce79fb8bffaea24c679fe5c7fa
Lucas Couto <lucascoutojp@gmail.com>
2024-11-14 21:54:00 +0900

    Add authentication middleware to todo routes for enhanced security

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,12 @@ Auth::routes();
 // get, post, put, delete, patch 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::group(['prefix' => 'todo'], function () {
-    Route::get('/create', [TodoController::class, 'create'])->name('todo.create');
-    Route::get('/edit/{todo}', [TodoController::class, 'edit'])->name('todo.edit');
-    Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
-    Route::patch('/update/{todo}', [TodoController::class, 'update'])->name('todo.update');
-    Route::delete('/destroy/{id}', [TodoController::class, 'destroy'])->name('todo.destroy');
+Route::middleware('auth')->group(function () {
+    Route::group(['prefix' => 'todo'], function () {
+        Route::get('/create', [TodoController::class, 'create'])->name('todo.create');
+        Route::get('/edit/{todo}', [TodoController::class, 'edit'])->name('todo.edit');
+        Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
+        Route::patch('/update/{todo}', [TodoController::class, 'update'])->name('todo.update');
+        Route::delete('/destroy/{id}', [TodoController::class, 'destroy'])->name('todo.destroy');
+    });
 });
